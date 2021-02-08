d76e9ca05f1e502e2524438baf22bd48c9260a1a
Brian Niedzialkoski <brianniedzialkoski@gmail.com>
2021-02-07 21:51:16 -0500

    Part 2 Complete

--- src/main/java/com/popularpenguin/coinbase/MainActivity.kt
+++ src/main/java/com/popularpenguin/coinbase/MainActivity.kt
@@ -2,19 +2,28 @@ package com.popularpenguin.coinbase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.popularpenguin.coinbase.databinding.ActivityMainBinding
+import com.popularpenguin.coinbase.repository.BitcoinTicker
 import com.popularpenguin.coinbase.util.Constants
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
+import javax.net.ssl.SSLSocketFactory
 
 // https://medium.com/@fahri.c93/android-tutorial-part-2-using-java-websocket-with-kotlin-dd3105bd3eed
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "MainActivity"
     private lateinit var webSocketClient: WebSocketClient
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onResume() {
@@ -31,12 +40,32 @@ class MainActivity : AppCompatActivity() {
         val coinbaseUri = URI(Constants.SOCKET_BASE_URL)
 
         createWebSocketClient(coinbaseUri)
+
+        val socketFactory: SSLSocketFactory = SSLSocketFactory.getDefault() as SSLSocketFactory
+
+        webSocketClient.setSocketFactory(socketFactory)
+        webSocketClient.connect()
     }
 
     private fun createWebSocketClient(coinbaseUri: URI) {
         webSocketClient = object : WebSocketClient(coinbaseUri) {
             override fun onOpen(handshakedata: ServerHandshake?) {
+                Log.d(TAG, "onOpen")
                 subscribe()
+            }
+
+            override fun onMessage(message: String?) {
+                Log.d(TAG, "onMessage: $message")
+                setUpBtcPriceText(message)
+            }
+
+            override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                Log.d(TAG, "onClose")
+                unsubscribe()
+            }
+
+            override fun onError(ex: Exception?) {
+                Log.e(TAG, "onError: ${ex?.message}")
             }
         }
     }
@@ -46,6 +75,24 @@ class MainActivity : AppCompatActivity() {
                 "{\n" +
                         "    \"type\": \"subscribe\",\n" +
                         "    \"channels\": [{ \"name\": \"ticker\", \"product_ids\": [\"BTC-EUR\"] }]\n" +
+                        "}"
+        )
+    }
+
+    private fun setUpBtcPriceText(message: String?) {
+        message?.let {
+            val moshi = Moshi.Builder().build()
+            val adapter: JsonAdapter<BitcoinTicker> = moshi.adapter(BitcoinTicker::class.java)
+            val bitcoin = adapter.fromJson(message)
+            runOnUiThread { binding.btcPriceTv.text = "1 BTC: ${bitcoin?.price} €" }
+        }
+    }
+
+    private fun unsubscribe() {
+        webSocketClient.send(
+                "{\n" +
+                        "    \"type\": \"unsubscribe\",\n" +
+                        "    \"channels\": [\"ticker\"]\n" +
                         "}"
         )
     }
